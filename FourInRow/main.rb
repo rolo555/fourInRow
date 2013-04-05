c3353940e915437d8a1c9e69bfd865c16c5ba04f
rolo <rolo555@gmail.com>
2013-04-04 20:27:58 -0400

    diagonal derecha

--- FourInRow/main.rb
+++ FourInRow/main.rb
@@ -27,11 +27,11 @@ def valid_row?(row, col, start_row)
   return false if pos_val == 0
   if(start_row <= 3)
     (start_row .. (start_row + 3)).each do |option_start|
-      return false if ((@@table[row][option_start] == pos_val * -1 ) or ( option_start > 6))
+      return false if (( option_start > 6) or (@@table[row][option_start] == pos_val * -1 ))
     end
   else
     ((start_row - 3) .. start_row).each do |option_start|
-      return false if ((@@table[row][option_start] == pos_val * -1) or (option_start < 0))
+      return false if ((option_start < 0) or (@@table[row][option_start] == pos_val * -1))
     end
   end
   true
@@ -42,12 +42,23 @@ def valid_col?(row, col, start_col)
   return false if pos_val == 0
   if(start_col < 3)
     (start_col .. (start_col + 3)).each do |option_start|
-      return false if ((@@table[option_start][col] ==  pos_val * -1) or (option_start > 5))
+      return false if ( (option_start > 5) or (@@table[option_start][col] ==  pos_val * -1))
     end
   else
     ((start_col - 3) .. start_col).each do |option_start|
-      return false if ((@@table[option_start][col] == pos_val * -1) or (option_start < 0))
+      return false if ( (option_start < 0) or (@@table[option_start][col] == pos_val * -1))
     end
+  end
+  true
+end
+
+def valid_right_diagonal?(row, col, start_row, start_col)
+  pos_val = @@table[row][col]
+  return false if pos_val == 0
+  3.times do
+    return false if ((start_row < 0 ) or (start_col > 6) or (start_row > 5) or (start_col < 0) or (@@table[start_row][start_col] == pos_val * -1))
+    start_row -= 1
+    start_col += 1
   end
   true
 end
@@ -83,6 +94,18 @@ def get_start_col(row, col)
   resp
 end
 
+def get_start_right_diagonal(row, col)
+  resp =[]
+  aux_row = row
+  aux_col = col
+  4.times do
+    resp << [aux_row, aux_col] if (valid_right_diagonal?(row, col, aux_row, aux_col))
+    aux_row += 1
+    aux_col -= 1
+  end
+  resp
+end
+
 def count_number_of_moves(row, col)
   moves = 0
   (row...6).each do |new_row|
@@ -110,7 +133,6 @@ def best_option_row(row, col)
         moves += count_number_of_moves(row, option_col)
       end
     end
-    #    puts "moves for #{row}, #{option}: #{moves}"
     number_of_best_moves += 1 if moves == best_move
     number_of_best_moves = 1 if moves < best_move
     best_move = moves if moves < best_move
@@ -127,7 +149,27 @@ def best_option_col(row, col)
   options.each do |option|
     moves = 0
     moves += count_number_of_moves((option - 3), col)
-    puts "moves for #{row}, #{option}: #{moves}"
+    number_of_best_moves += 1 if moves == best_move
+    number_of_best_moves = 1 if moves < best_move
+    best_move = moves if moves < best_move
+  end
+  number_of_best_moves * best_move
+end
+
+def best_option_right_diagonal(row, col)
+  options = get_start_right_diagonal(row, col)
+  return 0 if options.empty?
+  number_of_best_moves = 1
+  best_move = 99
+  options.each do |option|
+    aux_row = option[0]
+    aux_col = option[1]
+    moves = 0
+    3.times do
+      aux_row -= 1
+      aux_col += 1
+      moves += count_number_of_moves(aux_row, aux_col)
+    end
     number_of_best_moves += 1 if moves == best_move
     number_of_best_moves = 1 if moves < best_move
     best_move = moves if moves < best_move
@@ -148,10 +190,13 @@ end
 @@table[5][4] = -1
 @@table[5][5] = -1
 
-#puts valid_row?(5,6,6)
-#puts valid_col?(3,3,2)
-#options = get_start_col(3,3);
-#puts options
-puts best_option_row(3,4)
+@@row = 3
+@@col = 3
+
+puts "para [#{@@row}, #{@@col}]:"
+puts "mejor fila: #{best_option_row(@@row, @@col)}"
+puts "mejor columna: #{best_option_col(@@row, @@col)}"
+puts "mejor digaonal derecha: #{best_option_right_diagonal(@@row, @@col)}"
+
 show_table
 
